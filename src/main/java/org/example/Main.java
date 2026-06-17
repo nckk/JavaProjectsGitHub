f54edff0a74234f956545869ae6c806c67781a4f
KrishnaNallani <nckk@users.noreply.github.com>
2026-06-17 10:24:16 -0500

    Add Git fetch demo message to Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,6 +18,7 @@ public class Main {
             System.out.println("this comment is after creating first branch");
             System.out.println("this is done for PullRequest Demo");
             System.out.println("Git pull Demo for feature branch");
+            System.out.println("Git fetch demo for feature branch hahahah")
         }
     }
 }
